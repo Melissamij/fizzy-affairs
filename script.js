35464a4e35861e036b416fc323110e0520409fab
Melissamij <102660511+Melissamij@users.noreply.github.com>
2025-04-16 12:36:30 +0200

    header nav

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.hamburger-button').click(function(){
+    $('.mobile-menu').slideToggle(500);
+    $(this).toggleClass('active');
+  });
+
+// animate on scroll activate 
+AOS.init({
+    duration: 500
+});
