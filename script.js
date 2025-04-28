0de4fc8a5bcf54f4bc8411e1ed3c9ef05052b176
Melissamij <102660511+Melissamij@users.noreply.github.com>
2025-04-28 18:25:32 +0200

    hamburger fixed

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// Toggle mobile menu on hamburger click
+// Toggle mobile menu and icons
 $('.hamburger-button').click(function () {
-  $('.mobile-menu').slideToggle(500);
-  $(this).toggleClass('active');
-  $('.icon-bars').toggle();
-  $('.icon-close').toggle();
+  // If the menu is visible (menu is open)
+  if ($('.mobile-menu').is(':visible')) {
+    // Close the menu
+    $('.mobile-menu').slideUp(500);
+    // Change icon back to hamburger
+    $(this).find('.icon-close').hide();
+    $(this).find('.icon-bars').show();
+    // Change color back to white
+    $(this).css('color', 'var(--featured-text-white)');
+  } else {
+    // Open the menu
+    $('.mobile-menu').slideDown(500);
+    // Change icon to close (X)
+    $(this).find('.icon-bars').hide();
+    $(this).find('.icon-close').show();
+    // Change color to brown (menu open)
+    $(this).css('color', 'var(--bg-dark)');
+  }
 });
 
-// Close mobile menu when a link is clicked
+// When clicking a menu link: close everything cleanly
 $('.mobile-menu a').click(function () {
   $('.mobile-menu').slideUp(500);
-  $('.hamburger-button').removeClass('active');
-  $('.icon-bars').show();
-  $('.icon-close').hide();
+  $('.hamburger-button .icon-close').hide();
+  $('.hamburger-button .icon-bars').show();
+  $('.hamburger-button').css('color', 'var(--featured-text-white)');
 });
